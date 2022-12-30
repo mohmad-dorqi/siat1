f00bd40f564f8e4583d70eb8e5d3b55e0edaaead
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-12-30 19:52:04 +0330

    siat fro phone

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 
 export default function App() {
+  const image = { uri: "./assets/favicon.png" };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+     <ImageBackground source={require('./assets/favicon.png')} resizeMode="cover"  style={styles.image}>
+       <Text style={styles.text} >ورود به برنامه</Text>
+    </ImageBackground>
+    
+    
     </View>
   );
 }
@@ -13,8 +18,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
+    
   },
+  stu:{
+    backgroundColor: 'red'
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  text:{
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
+  
 });
