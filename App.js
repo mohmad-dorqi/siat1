89b58f3909ff85d0f9f354e9a66e5c7a21bf5594
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-12-31 20:14:18 +0330

    create login screen

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,ImageBackground, TextInput } from 'react-native';
+import Home from './components/Home';
 
 export default function App() {
   const image = { uri: "./assets/favicon.png" };
+  const [flag,setFlag]=useState(false)
+  if(flag){
+    return (
+    <Home/>
+    );
+  }
   return (
     <View style={styles.container}>
 
-     <ImageBackground source={require('./assets/favicon.png')} resizeMode="cover"  style={styles.image}>
-       <Text style={styles.text} >ورود به برنامه</Text>
+     <ImageBackground source={require('./assets/images/vrod.jpg')} resizeMode="repeat"  style={styles.image}>
+       <View style={styles.login}>
+         <Text style={styles.label}>ورود به سیات</Text>
+         
+         <TextInput style={styles.input}
+           keyboardType="numeric"
+         ></TextInput>
+       </View>
     </ImageBackground>
     
     
@@ -18,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-   
+    justifyContent: "center"
     
   },
   stu:{
@@ -26,15 +40,34 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
+    alignItems:'center',
+    textAlign:'center'
+    
   },
-  text:{
+  label:{
     color: "white",
-    fontSize: 42,
-    lineHeight: 84,
+    fontSize: 24,
+    lineHeight: 50,
+    
     fontWeight: "bold",
     textAlign: "center",
-    backgroundColor: "#000000c0"
+  
+  },
+  login:{
+    backgroundColor:'red',
+    width:350,
+    textAlign:'center',
+    borderRadius:10,
+    padding:8
+    
+  },
+  input:{
+    borderColor:'white',
+    borderWidth:2,
+    backgroundColor:'white',
+    borderRadius:5,
+    padding:2
   }
   
 });
