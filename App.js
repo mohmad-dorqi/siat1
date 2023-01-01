5447c7a1bf0fc05292a6e03e80101075a9f46342
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2023-01-01 18:26:49 +0330

    instal axios and update login form

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View,ImageBackground, TextInput } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground, TextInput, Pressable } from 'react-native';
 import Home from './components/Home';
 
 export default function App() {
   const image = { uri: "./assets/favicon.png" };
-  const [flag,setFlag]=useState(false)
+  const [flag,setFlag]=useState(false);
+  const [text,setText]=useState({
+    user:'',
+    password:''
+  });
+ 
   if(flag){
     return (
     <Home/>
@@ -16,11 +21,27 @@ export default function App() {
 
      <ImageBackground source={require('./assets/images/vrod.jpg')} resizeMode="repeat"  style={styles.image}>
        <View style={styles.login}>
-         <Text style={styles.label}>ورود به سیات</Text>
-         
+         <Text style={styles.title}>ورود به سیات</Text>
+        <Text style={styles.label}>
+           نام کاربری: 
+        </Text>
          <TextInput style={styles.input}
            keyboardType="numeric"
+           onChangeText={(user)=>setText({...text,password:user})}
          ></TextInput>
+          <Text style={styles.label}>
+           نام کاربری: 
+        </Text>
+         <TextInput style={styles.input}
+           keyboardType="numeric"
+           secureTextEntry={true}
+           onChangeText={(pass)=>setText({...text,password:pass})}
+         ></TextInput>
+
+         <Pressable>
+           <Text style={styles.text}>ورود </Text>
+         </Pressable>
+         
        </View>
     </ImageBackground>
     
@@ -45,17 +66,17 @@ const styles = StyleSheet.create({
     textAlign:'center'
     
   },
-  label:{
+  title:{
     color: "white",
     fontSize: 24,
     lineHeight: 50,
-    
+    marginBottom:10,
     fontWeight: "bold",
     textAlign: "center",
   
   },
   login:{
-    backgroundColor:'red',
+    backgroundColor:'#95d5b2',
     width:350,
     textAlign:'center',
     borderRadius:10,
@@ -67,7 +88,24 @@ const styles = StyleSheet.create({
     borderWidth:2,
     backgroundColor:'white',
     borderRadius:5,
-    padding:2
+    padding:2,
+    marginBottom:25
+  },
+  label:{
+    color:'black',
+    fontWeight: "bold"
+    
+  },
+  text:{
+    textAlign:'center',
+    marginBottom:15,
+    backgroundColor:'red',
+    marginLeft:90,
+    marginRight:90,
+    height:35,
+    padding:5,
+    fontSize:18,
+    borderRadius:5
   }
   
 });
